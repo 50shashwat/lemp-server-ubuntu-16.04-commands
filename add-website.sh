c526630615fb362f50330918fc0b8371414cca14
root <root@ip-172-31-4-180.us-west-2.compute.internal>
2017-08-09 02:52:10 +0000

    fixed add-website command

--- add-website.sh
+++ add-website.sh
@@ -3,6 +3,6 @@ sudo mkdir -p /var/www/$1/public_html
 sudo chown -R www-data:www-data /var/www/$1/public_html
 sudo chmod -R 755 /var/www/$1/public_html/
 echo "<html><head><title>Working</title></head><body><h2>Working</h2></body></html>" > /var/www/$1/public_html/index.html
-sudo echo "server {\n        listen 80;\n        listen [::]:80;\n       root /var/www/$1/public_html;\n        index index.php index.html index.htm index.nginx-debian.html;\n        server_name $1 www.$1;\n        location / {\n                try_files \$uri \$uri/ /index.php?args;\n        }\n		 location ~ \\.php\$ {\n        include snippets/fastcgi-php.conf;\n        fastcgi_pass unix:/run/php/php7.0-fpm.sock;\n    }\n    location ~ /\\.ht {\n        deny all;\n    }\n}\n" > /etc/nginx/sites-available/$1
-sudo ln -s /etc/nginx/sites-availabel/$1 /etc/nginx/sites-enabled/
+sudo echo "server {        listen 80;        listen [::]:80;       root /var/www/$1/public_html;        index index.php index.html index.htm index.nginx-debian.html;        server_name $1 www.$1;        location / {                try_files \$uri \$uri/ /index.php?args;        }		 location ~ \\.php\$ {        include snippets/fastcgi-php.conf;        fastcgi_pass unix:/run/php/php7.0-fpm.sock;    }    location ~ /\\.ht {        deny all;    }}" > /etc/nginx/sites-available/$1
+sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/
 sudo systemctl reload nginx

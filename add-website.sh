33c62a162dfab1308574a80076547df70fe088fb
Shashwat Singh <shashwat@kalpvaig.com>
2018-09-02 16:17:32 +0530

    fixed socket error for php7.2

--- add-website.sh
+++ add-website.sh
@@ -6,7 +6,7 @@ sudo mkdir -p /var/www/$1/public_html
 sudo chown -R www-data:www-data /var/www/$1/public_html
 sudo chmod -R 755 /var/www/$1/public_html/
 sudo echo -e "<html>\n\t<head>\n\t\t<title>Working</title>\n\t</head>\n\t<body>\n\t\t<h2>Working</h2>\n\t</body>\n</html>" > /var/www/$1/public_html/index.html
-sudo echo -e "server {\n\t listen 80;\n\tlisten [::]:80;\n\troot /var/www/$1/public_html;\n\tindex index.php index.html index.htm index.nginx-debian.html;\n\tserver_name $1 www.$1;\n\tlocation / {\n\t\ttry_files \$uri \$uri/ /index.php?\$args;\n\t}\n\tlocation ~ \\.php\$ {\n\t\tinclude snippets/fastcgi-php.conf;\n\t\tfastcgi_pass unix:/run/php/"$php_version"-fpm.sock;\n\t}\n\tlocation ~ /\\.ht {\n\t\tdeny all;\n\t}\n}" > /etc/nginx/sites-available/$1
+sudo echo -e "server {\n\t listen 80;\n\tlisten [::]:80;\n\troot /var/www/$1/public_html;\n\tindex index.php index.html index.htm index.nginx-debian.html;\n\tserver_name $1 www.$1;\n\tlocation / {\n\t\ttry_files \$uri \$uri/ /index.php?\$args;\n\t}\n\tlocation ~ \\.php\$ {\n\t\tinclude snippets/fastcgi-php.conf;\n\t\tfastcgi_pass unix:/run/php/php"$php_version"-fpm.sock;\n\t}\n\tlocation ~ /\\.ht {\n\t\tdeny all;\n\t}\n}" > /etc/nginx/sites-available/$1
 sudo ln -s /etc/nginx/sites-available/$1 /etc/nginx/sites-enabled/
 sudo systemctl reload nginx
 sudo service nginx reload
